91c079c3bd7b96242a8ef347a55b1b7a3002c4ec
mena17-stacks <mena0811x@gmail.com>
2023-02-20 12:11:40 +0100

    create an array of the words then used random number function

---
+++ variable.js
@@ -0,0 +1,11 @@
+
+const getComputerChoice= () ={
+    switch (getComputerChoice){
+        case 0:
+            'rock';
+        case 1:
+            'paper';
+        case 2:
+            'scissors';
+    }
+}
