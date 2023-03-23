e3c45fc6e3ca17baa9283155b60622e138a1ff4d
mena17-stacks <mena0811x@gmail.com>
2023-03-23 13:47:33 +0000

    button is connected

--- javascript2.js
+++ javascript2.js
@@ -2,88 +2,67 @@
 function game() {}
 let playerScore = 0;
 let computerScore = 0;
+let roundCount =0;
 
-const buttons = document.querySelectorAll('.btn');
-let rock = 'rock';
-let paper = 'paper';
-let scissors = 'scissors';
+window.onload = function(){ 
+document.getElementById('rock').onclick = player;
+document.getElementById('paper').onclick = player;
+document.getElementById('scissors').onclick = player;
+};
+
+function player(){
+   let playerChoice = this.id;
+   console.log("Player: " + playerChoice)
+}
 
 const words = ['rock', 'paper', 'scissors'];
-let getComputerChoice = words[Math.floor(Math.random() * words.length)]
-   console.log (getComputerChoice)
+let getComputerChoice = words[Math.floor(Math.random() * words.length)];
+   console.log (getComputerChoice);
 
 
 function playRound(playerSelection, computerSelection) {
    if (playerSelection===computerSelection){
      console.log( 'It\'s a tie!');
-      return;
-  }
+   }
   else if (playerSelection==='rock' && computerSelection==='paper'){
    computerScore++;
    console.log('Paper beats rock.');
-   return;
-  }
+   }
   else if (playerSelection=== 'rock' && computerSelection=== 'scissors'){
    playerScore++;
    console.log('Rock beats scissors');
-   return;
-  }
+   }
   else if (playerSelection==='paper' && computerSelection=== 'paper'){
      console.log('It\'s a tie!');
-     return;
-  }
+   }
   else if (playerSelection==='paper' && computerSelection=== 'rock'){
    playerScore++;  
    console.log('paper beats rock');
-   return;
-  }
+   }
   else if (playerSelection==='paper' && computerSelection=== 'scissors'){
    computerScore++;
    console.log('scissors beats paper');
-   return;
-  }
+   }
   else if (playerSelection==='scissors' && computerSelection=== 'scissors'){
    console.log('It\'s a tie!');
-   return;
-  }
+   }
   else if (playerSelection==='scissors' && computerSelection=== 'paper'){
    playerScore++;
    console.log('scissors beats paper');
-   return;
   }
   else if (playerSelection==='scissors' && computerSelection=== 'rock'){
    computerScore++;
    console.log('rock beats scissors');
-   return;
-  }
+   }
   else {
      console.log('this is not an adequate response. Choose rock, paper or scissors.');
-     return;
    }
 
-console.log(playRound(playerSelection, computerSelection));
+   console.log(playRound(playerSelection, computerSelection));
 
-game();
-console.log(`Player pick: ${playerSelection}`);
-console.log(`Computer pick: ${computerSelection}`);
-console.log(`Computer: ${computerScore}`);
-console.log(`Player: ${playerScore}`);
-
-function checkGame(){
-   if (playerScore === '5'){
-      console.log('Game Over')
-      console.log ('Congrats! You win!')
-   }
-   else if (computerScore === '5'){
-      console.log('Game Over')
-      console.log ('Computer wins!')
-   }
-
-   else{
-      for (let i = 0; i < 5; i++) {
-         playRound()
-      };
-   }
+   game();
+   console.log(`Player pick: ${playerSelection}`);
+   console.log(`Computer pick: ${computerSelection}`);
+   console.log(`Computer: ${computerScore}`);
+   console.log(`Player: ${playerScore}`);
 }
-}
-
